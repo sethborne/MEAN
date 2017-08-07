02ca047ee1c86d18ddd299d0f777a09602072d47
Seth Borne <sethborne@gmail.com>
2017-08-06 22:00:09 -0700

    JavaScript - JS Fundamentals - 12 - Assignment 01 - JS Basics - First Commit - Basic 01

---
+++ 01_JAVASCRIPT/B_JS_FUNDAMENTALS/12_ASSIGNMENT_01_JS_BASICS_I/12_ASSIGNMENT_01_JS_BASICS_I.js
@@ -0,0 +1,20 @@
+// Create a variable x as an empty array []. Log this array to your console.
+
+let x = [];
+console.log(x);
+//should see []
+
+// Use .push() to add three strings to your array: 'coding', 'dojo', 'rocks'.
+
+x.push('coding');
+x.push('dojo');
+x.push('rocks');
+console.log(x);
+
+// Use .pop() to remove the final element of your array.
+
+x.pop(2);
+
+// Log the final value of x, what is it?
+// should be ['coding', 'dojo']
+console.log(x);

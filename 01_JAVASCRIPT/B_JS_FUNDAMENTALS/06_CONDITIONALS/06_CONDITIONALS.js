6e5b1e560a6302e397ede5a48a8019775f9cd802
Seth Borne <sethborne@gmail.com>
2017-08-06 20:44:58 -0700

    JavaScript - JS Fundamentals - Lesson 06 - First Commit

---
+++ 01_JAVASCRIPT/B_JS_FUNDAMENTALS/06_CONDITIONALS/06_CONDITIONALS.js
@@ -0,0 +1,22 @@
+let diet = true;
+if (diet) {
+    console.log('Don\'t eat cake');
+} else {
+    console.log('Eat cake');
+}
+
+let score = 3;
+let totalScore = 5;
+if (score === 5) {
+    console.log(`You got 5 out of ${totalScore}`);
+} else if (score === 4) {
+    console.log(`You got 4 out of ${totalScore}`);
+} else if (score === 3) {
+    console.log(`You got 3 out of ${totalScore}`);
+} else if (score === 2) {
+    console.log(`You got 2 out of ${totalScore}`);
+} else if (score === 1) {
+    console.log(`You got 1 out of ${totalScore}`);
+};
+
+le

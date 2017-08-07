9ef7b7f8b73f930ddcf7ab821ae5c9d174f0f4c0
Seth Borne <sethborne@gmail.com>
2017-08-06 19:59:54 -0700

    JavaScript - JS Fundamentals - Lession 03 - First Commit

---
+++ 01_JAVASCRIPT/B_JS_FUNDAMENTALS/03_VARIABLES/03_VARIABLES.js
@@ -0,0 +1,16 @@
+var myAge = 29;
+
+var myName = "Seth";
+
+var myBirthday;
+
+// console.log(myName);
+// console.log(myAge);
+// console.log(myBirthday);
+
+var x = [1, 2, 3];
+var y = x;
+
+x.push("Hello World");
+console.log(x);
+console.log(y);

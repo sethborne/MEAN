ee667534fb9bb997120ba8c52259215f47398c50
Seth Borne <sethborne@gmail.com>
2017-08-12 11:00:06 -0700

    Express - Lesson 04 - Views - Second Commit

--- 03_EXPRESS/A_EXPRESS/04_VIEWS/server.js
+++ 03_EXPRESS/A_EXPRESS/04_VIEWS/server.js
@@ -1,10 +1,26 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + "/static"));
+// serve static files, pre EJS
+// app.use(express.static(__dirname + "/static"));
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
 app.get('/', function(request, response) {
     response.send("Hello Express")
+})
+
+app.get("/users", function(request, response) {
+    // info
+    var usersArray = [
+        { name: "Michael", email: "michael@codingdojo.com" },
+        { name: "Jay", email: "jay@codingdojo.com" },
+        { name: "Brendan", email: "brendan@codingdojo.com" },
+        { name: "Andrew", email: "andrew@codingdojo.com" }
+        // {name: "", email: ""}
+    ];
+    response.render("users", { users: usersArray });
 })
 
 app.listen(8000, function() {

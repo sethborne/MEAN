8f0693e53b02748f5b1bf507fe57e8e6b886ef02
Seth Borne <sethborne@gmail.com>
2017-08-18 10:18:11 -0700

    MongoDB - Intro to Mongoose - Lesson 06 - App 03 - First Commit

---
+++ 04_MONGODB/C_INTERMEDIATE_MONGOOSE/06_CONTROLLERS/server.js
@@ -0,0 +1,34 @@
+// require express and path
+var express = require("express");
+var path = require("path");
+// create the express app
+var app = express();
+// require bodyParser since we need to handle post data for adding a user
+var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/quotes');
+var QuoteSchema = new mongoose.Schema({
+  name: String,
+  quote: String
+})
+var Quote = mongoose.model('Quote', QuoteSchema);
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// chalk
+var chalk = require("chalk");
+// static content 
+app.use(express.static(path.join(__dirname, "./static")));
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+// bring in your routes
+var routeSetter = require("./server/config/routes.js");
+routeSetter(app);
+
+// tell the express app to listen on port 8000
+let portNumber = 8000;
+app.listen(portNumber, function () {
+  console.log(chalk.white.bgGreen(` Listening on Port ${portNumber} `));
+})

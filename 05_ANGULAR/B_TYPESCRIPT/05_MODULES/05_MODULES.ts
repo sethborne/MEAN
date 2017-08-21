79351d7d5ed717501d7d7e1f9999394ff0ae06d4
Seth Borne <sethborne@gmail.com>
2017-08-21 14:52:03 -0700

    Angular - TypeScript - Lesson 05 - Modules - First Commit

---
+++ 05_ANGULAR/B_TYPESCRIPT/05_MODULES/05_MODULES.ts
@@ -0,0 +1,5 @@
+// patterns
+
+import * from "libraryName"; // this will import all from a globally installed library
+import { specificLibraryObject } from 'filepath/LibraryName'; // import 1 thing from specificLibraryObject
+import { libraryObjectA, libraryObjectB } from 'filePath/LibraryName' // import 2 things from specificLibraryObject  

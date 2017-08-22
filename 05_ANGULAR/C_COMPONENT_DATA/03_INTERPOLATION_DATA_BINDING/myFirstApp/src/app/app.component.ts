d00930e7fd87079236119263f30eeca5beb6b4ff
Seth Borne <sethborne@gmail.com>
2017-08-21 20:41:38 -0700

    Angular - Component Data - Lesson 03 - Interpolation and Data Binding - First Commit

---
+++ 05_ANGULAR/C_COMPONENT_DATA/03_INTERPOLATION_DATA_BINDING/myFirstApp/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  x: number = 7;
+  y: number = 9;
+  myStr: string = "abc";
+}

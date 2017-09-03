5032832dab59b2bc5c22fb1f20c799848b556ee3
Seth Borne <sethborne@gmail.com>
2017-09-03 09:01:27 -0700

    Angular - Routing - Lesson 01 - Routing - Second Commit

--- 05_ANGULAR/G_ROUTING/01_ROUTING/Lesson01Routing/src/app/app-routing.module.ts
+++ 05_ANGULAR/G_ROUTING/01_ROUTING/Lesson01Routing/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LandingComponent } from "./landing/landing.component";
+import { HomeComponent } from "./home/home.component";
+import { NotesPrivateComponent } from "./notes-private/notes-private.component";
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  // { path: '', children: [] },
+  { path: '', pathMatch: 'full', component: LandingComponent },
+  { path: 'home', component: HomeComponent },
+  // { path: 'notes/private', component: NotesPrivateComponent },
+  // { path: 'gohome', redirectTo: '/home' },
 ];
 
 @NgModule({
